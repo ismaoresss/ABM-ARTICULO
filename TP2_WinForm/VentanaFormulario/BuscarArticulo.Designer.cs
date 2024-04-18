f0465f6a54bf6af9ff19ccb556dfc310415df76d
FacundoPino <facunahuelp@gmail.com>
2024-04-17 22:25:48 -0300

    agregue una venta de busqueda articulos

---
+++ TP2_WinForm/VentanaFormulario/BuscarArticulo.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace TP2_WinForm.VentanaFormulario
+{
+    partial class BuscarArticulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BuscarArticulo));
+            this.lbl_BuscarArticulo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_BuscarArticulo
+            // 
+            this.lbl_BuscarArticulo.AutoSize = true;
+            this.lbl_BuscarArticulo.Font = new System.Drawing.Font("Stencil", 20.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_BuscarArticulo.ForeColor = System.Drawing.Color.White;
+            this.lbl_BuscarArticulo.Location = new System.Drawing.Point(229, 34);
+            this.lbl_BuscarArticulo.Name = "lbl_BuscarArticulo";
+            this.lbl_BuscarArticulo.Size = new System.Drawing.Size(342, 32);
+            this.lbl_BuscarArticulo.TabIndex = 0;
+            this.lbl_BuscarArticulo.Text = "BUSQUEDA DE ARTICULOS";
+            this.lbl_BuscarArticulo.Click += new System.EventHandler(this.lbl_BuscarArticulo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SlateBlue;
+            this.panel1.Controls.Add(this.lbl_BuscarArticulo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // BuscarArticulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BuscarArticulo";
+            this.Text = "BuscarArticulo";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_BuscarArticulo;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ TP2_WinForm/VentanaFormulario/BuscarArticulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP2_WinForm.VentanaFormulario
+{
+    public partial class BuscarArticulo : Form
+    {
+        public BuscarArticulo()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_BuscarArticulo_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
